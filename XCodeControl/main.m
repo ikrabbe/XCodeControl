b30ee477118da2d322f7fd5b496956a49bc04a17
Ingo Krabbe <ikrabbe.ask@gmail.com>
2015-05-28 10:06:52 +0200

    basic macos objective-c cli tool

---
+++ XCodeControl/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XCodeControl
+//
+//  Created by Ingo Krabbe on 28.05.15.
+//  Copyright (c) 2015 Ingo Krabbe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
